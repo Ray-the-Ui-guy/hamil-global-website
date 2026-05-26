9b6daee51c6de17a7a351d51f789420773359f3a
Heritage Isaac <dipedeji@gmail.com>
2026-05-26 17:03:26 +0100

    Fine-tune beacon: 176px right, top 29%

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -338,7 +338,7 @@ function Index() {
               <img src={sectionGlobalMap} alt="Global map" className="w-full h-auto object-contain opacity-90 hover:opacity-100 transition-opacity duration-700" />
               
               {/* Blinking Korea HQ Marker */}
-              <div className="absolute w-3 h-3 md:w-4 md:h-4 bg-green-500 rounded-full top-[30%] left-[22%] ml-[160px] z-10" style={{boxShadow: '0 0 15px rgba(34,197,94,0.8)'}}>
+              <div className="absolute w-3 h-3 md:w-4 md:h-4 bg-green-500 rounded-full top-[29%] left-[22%] ml-[176px] z-10" style={{boxShadow: '0 0 15px rgba(34,197,94,0.8)'}}>
                 <div className="absolute inset-0 bg-green-400 rounded-full animate-ping"></div>
               </div>
             </div>
